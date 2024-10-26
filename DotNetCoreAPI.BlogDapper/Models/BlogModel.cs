2c0c20f97d9ffec4e915ef2b89d42eb1a03bb7f5
Myo Thet <mr.myothet@gmail.com>
2024-10-26 21:16:21 +0700

    Add BlogModel

---
+++ DotNetCoreAPI.BlogDapper/Models/BlogModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCoreAPI.BlogDapper.Models;
+
+public class BlogModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Content { get; set; }
+}
