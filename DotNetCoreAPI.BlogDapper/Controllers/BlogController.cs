a16d9b1792beb72f02e53b46d66f4c8903037531
Myo Thet <mr.myothet@gmail.com>
2024-11-03 21:50:26 +0700

    Add Blog Controller

---
+++ DotNetCoreAPI.BlogDapper/Controllers/BlogController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebApi.BlogDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase { }
+}
